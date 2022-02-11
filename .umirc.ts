a62658a58822a025ba27c58bdaefa0616b2164da
多牧 <xuzhi.mxz@antfin.com>
2022-02-11 14:28:16 +0800

    site: still using docs as site root

--- .umirc.ts
+++ .umirc.ts
@@ -9,5 +9,5 @@ export default defineConfig({
   title: 'G Paint - An easy drawing board powered by @antv/g',
   hash: true,
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-  outputPath: 'sites',
+  outputPath: 'docs',
 });
